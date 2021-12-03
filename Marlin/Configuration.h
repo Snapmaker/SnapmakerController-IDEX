7ba7c67a9ed6bb67542aec2b4399708d41194d75
xhycode <731374786@qq.com>
2021-12-03 11:44:09 +0800

    Feature: set heater max temp to 330, and hotend overshoot to 20

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -536,8 +536,8 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 320
-#define HEATER_1_MAXTEMP 320
+#define HEATER_0_MAXTEMP 330
+#define HEATER_1_MAXTEMP 330
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
@@ -553,7 +553,7 @@
  * (especially before PID tuning). Setting the target temperature too close to MAXTEMP guarantees
  * a MAXTEMP shutdown! Use these values to forbid temperatures being set too close to MAXTEMP.
  */
-#define HOTEND_OVERSHOOT 15   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
+#define HOTEND_OVERSHOOT 20   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
 #define BED_OVERSHOOT    10   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
 #define COOLER_OVERSHOOT  2   // (°C) Forbid temperatures closer than OVERSHOOT
 
