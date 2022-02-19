5586e527609f394690de641f0f98e00cc5172ffd
xhycode <731374786@qq.com>
2022-02-19 16:48:48 +0800

    Feature: upload the SACP code

---
+++ snapmaker/protocol/protocol_sacp.cpp
@@ -0,0 +1,115 @@
+#include "protocol_sacp.h"
+#include <functional>
+#include "HAL.h"
+#include "../../Marlin/src/core/serial.h"
+
+ProtocolSACP protocol_sacp;
+
+static uint8_t sacp_calc_crc8(uint8_t *buffer, uint16_t len) {
+  int crc = 0x00;
+  int poly = 0x07;
+  for (int i = 0; i < len; i++) {
+    for (int j = 0; j < 8; j++) {
+      bool bit = ((buffer[i] >> (7 - j) & 1) == 1);
+      bool c07 = ((crc >> 7 & 1) == 1);
+      crc <<= 1;
+      if (c07 ^ bit) {
+        crc ^= poly;
+      }
+    }
+  }
+  crc &= 0xff;
+  return crc;
+}
+
+uint16_t calc_checksum(uint8_t *buffer, uint16_t length) {
+  uint32_t volatile checksum = 0;
+
+  if (!length || !buffer)
+    return 0;
+
+  for (int j = 0; j < (length - 1); j = j + 2)
+    checksum += (uint32_t)(buffer[j] << 8 | buffer[j + 1]);
+
+  if (length % 2)
+    checksum += buffer[length - 1];
+
+  while (checksum > 0xffff)
+    checksum = ((checksum >> 16) & 0xffff) + (checksum & 0xffff);
+
+  checksum = ~checksum;
+
+  return (uint16_t)checksum;
+}
+
+ErrCode ProtocolSACP::parse(uint8_t *data, uint16_t len, SACP_param_t &out) {
+  uint8_t *parse_buff = out.buff;
+  if (parse_buff[0] != SACP_PDU_SOF_H) {
+    out.lenght = 0;
+  }
+  for (uint16_t i = 0; i < len; i++) {
+    uint8_t ch = data[i];
+    if (out.lenght == 0) {
+      if (ch == SACP_PDU_SOF_H) {
+        parse_buff[out.lenght++] = ch;
+      }
+    } else if (out.lenght == 1) {
+      if (ch == SACP_PDU_SOF_L) {
+        parse_buff[out.lenght++] = ch;
+      } else {
+        out.lenght = 0;
+      }
+    } else {
+      parse_buff[out.lenght++] = ch;
+    }
+
+    if (out.lenght == 7) {
+      if (sacp_calc_crc8(parse_buff, 6) != parse_buff[6]) {
+        out.lenght = 0;
+      }
+    } else {
+      uint16_t data_len = (parse_buff[3] << 8 | parse_buff[2]);
+      uint16_t total_len = data_len + 7;
+      if (out.lenght == total_len) {
+        uint16_t checksum = calc_checksum(&parse_buff[7], data_len - 2);
+        uint16_t checksum1 = (parse_buff[total_len - 1] << 8) | parse_buff[total_len - 2];
+        if (checksum == checksum1) {
+          out.lenght = 0;
+          return E_SUCCESS;
+        } else {
+          out.lenght = 0;
+          return E_PARAM;
+        }
+      } else if (out.lenght > total_len) {
+        out.lenght = 0;
+        return E_PARAM;
+      }
+    }
+  }
+  return E_IN_PROGRESS;
+}
+
+
+uint16_t ProtocolSACP::package(SACP_head_base_t head, uint8_t *in_data, uint16_t length, uint8_t *out_data) {
+  uint16_t data_len = (length + 8); // header 6 byte, checknum 2byte
+  SACP_struct_t *out =  (SACP_struct_t *)out_data;
+  out->sof_h = SACP_PDU_SOF_H;
+  out->sof_l = SACP_PDU_SOF_L;
+  out->length = data_len;
+  out->version = SACP_VERSION;
+  out->recever_id = head.recever_id;
+  out->crc8 = sacp_calc_crc8(out_data, 6);
+  out->sender_id = SACP_ID_CONTROLLER;
+  out->arrt.type = head.attribute;
+  out->sequence = head.sequence;
+  out->command_set = head.command_set;
+  out->command_id = head.command_id;
+  for (uint16_t i = 0; i < length; i++) {
+    out->data[i] = in_data[i];
+  }
+  uint16_t checksum = calc_checksum(&out_data[7], data_len - 2);  // - checknum 2 byte
+  length = sizeof(SACP_struct_t) + length;
+  out_data[length++] = (uint8_t)(checksum & 0x00FF);
+  out_data[length++] = (uint8_t)(checksum>>8);
+  return length;
+}
---
+++ snapmaker/protocol/protocol_sacp.h
@@ -0,0 +1,70 @@
+#ifndef PROTOCOL_ASCP_H
+#define PROTOCOL_ASCP_H
+
+#include "../J1/common_type.h"
+#include <functional>
+// protocol relative macros
+#define SACP_PDU_SOF_H   0xAA
+#define SACP_PDU_SOF_L   0x55
+#define SACP_VERSION     0x01
+
+#define SACP_ID_PC         0
+#define SACP_ID_CONTROLLER 1
+#define SACP_ID_HMI        2
+
+#define PACK_PARSE_MAX_SIZE 512
+#define PACK_PACKET_MAX_SIZE 1024
+
+#pragma pack(1)
+typedef struct {
+  uint8_t sof_h;
+  uint8_t sof_l;
+  uint16_t length;
+  uint8_t version;  // 0x01
+  uint8_t recever_id;
+  uint8_t crc8;
+  uint8_t sender_id;
+  union {
+    struct {
+      uint8_t type: 1;  // 0 - request, 1 - ack
+      uint8_t reserve : 7;
+    } arrt;
+  };
+  uint16_t sequence;
+  uint8_t command_set;
+  uint8_t command_id;
+  uint8_t data[];
+} SACP_struct_t;
+
+typedef struct {
+  uint8_t recever_id;
+  uint8_t attribute;
+  uint16_t sequence;
+  uint8_t command_set;
+  uint8_t command_id;
+} SACP_head_base_t;
+
+typedef struct {
+  uint16_t lenght;  // The total length of data
+  union {
+    uint8_t buff[PACK_PARSE_MAX_SIZE];
+    SACP_struct_t sacp;
+  };
+} SACP_param_t;
+
+
+
+#pragma pack()
+
+class ProtocolSACP {
+  public:
+    ErrCode parse(uint8_t *data, uint16_t len, SACP_param_t &out);
+    // Package the incoming data
+    uint16_t package(SACP_head_base_t head, uint8_t *in_data, uint16_t length, uint8_t *out_data);
+    uint16_t sequence_pop() {return sequence++;}
+  private:
+    uint32_t sequence = 0;
+};
+
+extern ProtocolSACP protocol_sacp;
+#endif
