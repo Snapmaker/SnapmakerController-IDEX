4ef2b51941fb404de387e01d6d0af51c08ebc406
xhycode <731374786@qq.com>
2022-06-08 16:17:25 +0800

    Feature: SC move log chenge to I

--- snapmaker/module/motion_control.cpp
+++ snapmaker/module/motion_control.cpp
@@ -30,34 +30,34 @@ void MotionControl::blocking_move_to(float x, float y, float z, float feedrate) 
 ErrCode MotionControl::move_axis(mobile_instruction_t *move) {
   xyze_pos_t xyze = current_position;
   float save_feedrate = feedrate_mm_s;
-  LOG_V("sc req move to");
+  LOG_I("sc req move to");
   for (uint8_t i = 0; i < move->axis_count; i++) {
     axis_move_t *axis_move = &(&move->axis_move)[i];
     switch (axis_move->axis) {
       case AXIS_X1:
         if (active_extruder == 0) {
           xyze.x = INT_TO_FLOAT(axis_move->distance);
-          LOG_V(" x:%f", xyze.x);
+          LOG_I(" x:%f", xyze.x);
         }
         break;
       case AXIS_X2:  // Support activities only one head movement
         if (active_extruder == 1) {
           xyze.x = INT_TO_FLOAT(axis_move->distance);
-          LOG_V(" x2:%f", xyze.x);
+          LOG_I(" x2:%f", xyze.x);
         }
         break;
       case AXIS_Y1:
         xyze.y += INT_TO_FLOAT(axis_move->distance);
-        LOG_V(" y:%f", xyze.y);
+        LOG_I(" y:%f", xyze.y);
         break;
       case AXIS_Z1:
         xyze.z += INT_TO_FLOAT(axis_move->distance);
-        LOG_V(" z:%f", xyze.z);
+        LOG_I(" z:%f", xyze.z);
         break;
     }
   }
   uint16_t speed = *((uint16_t*)((uint8_t*)move + 1 + (move->axis_count * sizeof(axis_move_t))));
-  LOG_V(" f:%d\n", speed);
+  LOG_I(" f:%d\n", speed);
   feedrate_mm_s = speed ? MMM_TO_MMS(speed) : feedrate_mm_s;
   blocking_move_to(xyze.x, xyze.y, xyze.z, feedrate_mm_s);
   feedrate_mm_s = save_feedrate;
@@ -69,7 +69,7 @@ ErrCode MotionControl::move_axis_to(mobile_instruction_t *move) {
   xyze_pos_t xyze = current_position;
   bool is_logical = !(*((uint8_t*)move + (3 + (move->axis_count * sizeof(axis_move_t)))));
   float save_feedrate = feedrate_mm_s;
-  SERIAL_ECHOPAIR("sc req move");
+  LOG_I("sc req move");
   if (is_logical) {
     xyze = current_position.asLogical();
   }
@@ -79,29 +79,29 @@ ErrCode MotionControl::move_axis_to(mobile_instruction_t *move) {
       case AXIS_X1:
         if (active_extruder == 0) {
           xyze.x = INT_TO_FLOAT(axis_move->distance);
-          LOG_V(" x:%f", xyze.x);
+          LOG_I(" x:%f", xyze.x);
         }
         break;
       case AXIS_X2:  // Support activities only one head movement
         if (active_extruder == 1) {
           xyze.x = INT_TO_FLOAT(axis_move->distance);
-          LOG_V(" x2:%f", xyze.x);
+          LOG_I(" x2:%f", xyze.x);
         }
         break;
       case AXIS_Y1:
         xyze.y = INT_TO_FLOAT(axis_move->distance);
-        LOG_V(" y:%f", xyze.y);
+        LOG_I(" y:%f", xyze.y);
         break;
       case AXIS_Z1:
         xyze.z = INT_TO_FLOAT(axis_move->distance);
-        LOG_V(" z:%f", xyze.z);
+        LOG_I(" z:%f", xyze.z);
         break;
     }
   }
   uint8_t *data = ((uint8_t*)move + (1 + (move->axis_count * sizeof(axis_move_t))));
   uint16_t speed = *((uint16_t*)data);
-  SERIAL_ECHOPAIR(" f:", speed);
-  SERIAL_ECHOLNPAIR(" is_logical:", is_logical);
+  LOG_I(" f:%d", speed);
+  LOG_I(" is_logical:%d\n", is_logical);
 
   feedrate_mm_s = speed ? speed : feedrate_mm_s;
   if (is_logical) {
