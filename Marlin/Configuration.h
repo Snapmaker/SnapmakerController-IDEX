6a0429fd4a3ebb31b9483b50337b6089c70a1616
xhycode <731374786@qq..com>
2022-07-27 10:29:03 +0800

    Feature: change default hotend temperature to 205

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1838,7 +1838,7 @@
 // Preheat Constants - Up to 5 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 180
+#define PREHEAT_1_TEMP_HOTEND 205
 #define PREHEAT_1_TEMP_BED     70
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
