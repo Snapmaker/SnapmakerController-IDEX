fee94082c3a36d214f0df90b48bd6c376feb77e3
zhangjiefeng <zjief1992@gmail.com>
2022-09-19 15:01:21 +0800

    Fix the data exception of thread switching

--- snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/port.c
+++ snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/port.c
@@ -448,9 +448,9 @@ void __exc_pendsv( void )
 	"	ldr	r3, pxCurrentTCBConst			\n" /* Get the location of the current TCB. */
 	"	ldr	r2, [r3]						\n"
 	"										\n"
-//	"	tst r14, #0x10						\n" /* Is the task using the FPU context?  If so, push high vfp registers. */
-//	"	it eq								\n"
-//	"	vstmdbeq r0!, {s16-s31}				\n"
+	"	tst r14, #0x10						\n" /* Is the task using the FPU context?  If so, push high vfp registers. */
+	"	it eq								\n"
+	"	vstmdbeq r0!, {s16-s31}				\n"
 	"										\n"
 	"	stmdb r0!, {r4-r11, r14}			\n" /* Save the core registers. */
 	"	str r0, [r2]						\n" /* Save the new top of stack into the first member of the TCB. */
@@ -470,9 +470,9 @@ void __exc_pendsv( void )
 	"										\n"
 	"	ldmia r0!, {r4-r11, r14}			\n" /* Pop the core registers. */
 	"										\n"
-//	"	tst r14, #0x10						\n" /* Is the task using the FPU context?  If so, pop the high vfp registers too. */
-//	"	it eq								\n"
-//	"	vldmiaeq r0!, {s16-s31}				\n"
+	"	tst r14, #0x10						\n" /* Is the task using the FPU context?  If so, pop the high vfp registers too. */
+	"	it eq								\n"
+	"	vldmiaeq r0!, {s16-s31}				\n"
 	"										\n"
 	"	msr psp, r0							\n"
 	"	isb									\n"
