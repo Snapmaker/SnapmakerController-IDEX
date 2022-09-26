c63da37c4aeb7961a5afd05155dd3faee87892a4
747lulu747 <a747lulu747@126.com>
2022-09-26 18:21:59 +0800

    Fix: restore thermal PID to preventing thermal error

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -581,9 +581,12 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 14.18
-    #define DEFAULT_Ki 0.80
-    #define DEFAULT_Kd 62.45
+    // #define DEFAULT_Kp 14.18
+    // #define DEFAULT_Ki 0.80
+    // #define DEFAULT_Kd 62.45
+    #define DEFAULT_Kp  22.20
+    #define DEFAULT_Ki   1.08
+    #define DEFAULT_Kd 114.00
   #endif
 #endif // PIDTEMP
 
