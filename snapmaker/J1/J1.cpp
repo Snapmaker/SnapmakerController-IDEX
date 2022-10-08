7734e7de2df0e577202b9254fd30f16081dd717d
747lulu747 <a747lulu747@126.com>
2022-10-08 11:25:17 +0800

    Feat: add main task

--- snapmaker/J1/J1.cpp
+++ snapmaker/J1/J1.cpp
@@ -5,6 +5,34 @@
 #include "switch_detect.h"
 #include "../module/update.h"
 #include "../module/fdm.h"
+#include "../event/event_printer.h"
+#include "../event/event_exception.h"
+#include "../module/print_control.h"
+#include "../../Marlin/src/module/temperature.h"
+#include "../module/exception.h"
+
+
+void j1_main_task(void *args) {
+  uint32_t syslog_timeout = millis();
+
+  while(1) {
+    printer_event_loop();
+    exception_event_loop();
+    print_control.loop();
+
+    if (ELAPSED(millis(), syslog_timeout)) {
+      syslog_timeout = millis() + 20000;
+      LOG_I("c0: %d/t0: %d, c1: %d/t1: %d, cb: %d/tb: %d\n",
+        (int)thermalManager.degHotend(0), thermalManager.degTargetHotend(0),
+        (int)thermalManager.degHotend(1), thermalManager.degTargetHotend(1),
+        (int)thermalManager.degBed(), thermalManager.degTargetBed());
+      LOG_I("sta: %u, excep sta: 0x%x, excep beh: 0x%x\n", system_service.get_status(),
+        exception_server.get_exception(), exception_server.get_behavior());
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(5));
+  }
+}
 
 void J1_setup() {
   update_server.init();
@@ -13,4 +41,12 @@ void J1_setup() {
   debug.init();
   subscribe_init();
   event_init();
+
+  BaseType_t ret = xTaskCreate(j1_main_task, "j1_main_task", 1024, NULL, 5, NULL);
+  if (ret != pdPASS) {
+    SERIAL_ECHO("Failed to create j1_main_task!\n");
+  }
+  else {
+    SERIAL_ECHO("Created j1_main_task task!\n");
+  }
 }
