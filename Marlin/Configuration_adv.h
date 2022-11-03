d411e762767aa8bd9a873c37d1f5146c292ee31e
747lulu747 <a747lulu747@126.com>
2022-11-03 19:48:24 +0800

    Feat: default K set to 0.02

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1872,7 +1872,7 @@
 #define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.04    // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0.02    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
   #define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
 #endif
