cb3e642b46d7db12e115a5d6febaa08beb328afc
747lulu747 <a747lulu747@126.com>
2023-05-06 15:03:28 +0800

    Feat: change version

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -21,7 +21,7 @@
  */
 #pragma once
 
-#define J1_BUILD_VERSION "2.2.8-E_Double_pos"
+#define J1_BUILD_VERSION "2.2.8-E_Double_pos_test"
 
 /**
  * Release version. Leave the Marlin version or apply a custom scheme.
